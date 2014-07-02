de8a71c04dc727c7e52c5ef3ea0394662fa7cb7a
Daniel Lamb <daniellmb@hotmail.com>
2014-07-02 06:24:29 -0700

    gulp template updates

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -15,28 +15,42 @@ var gulp = require('gulp'),
     complexity = require('gulp-complexity'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    karma = require('gulp-karma');
+    karma = require('gulp-karma'),
+    source = '<%= baseFileName %>.js',
+    sourceMin = '<%= baseFileName %>.min.js',
+    sourceAndSpecs = [source, 'test/spec/*.spec.js'];
 
-gulp.task('default', function () {
-  return gulp.src([
-      '<%= baseFileName %>.js',
-      'test/spec/*.spec.js'
-    ])
+gulp.task('lint', function () {
+  return gulp.src(sourceAndSpecs)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('gpa', function () {
+  return gulp.src(sourceAndSpecs)
     .pipe(complexity({
       cyclomatic: [8],
       halstead: [9],
       maintainability: [100]
-    }))
+    }));
+});
+
+gulp.task('test', function () {
+  return gulp.src(sourceAndSpecs)
     .pipe(karma({
       configFile: 'test/karma.conf.js'
-    }))
-    .pipe(rename('<%= baseFileName %>.min.js'))
+    }));
+});
+
+gulp.task('min', function () {
+  return gulp.src(source)
+    .pipe(rename(sourceMin))
     .pipe(uglify({
       preserveComments: 'some',
       outSourceMap: true
     }))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('default', ['lint', 'gpa', 'test', 'min']);
